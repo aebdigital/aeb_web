f5f82c31b470b780c13545514ea3c21a2b86154e
Alexander Hidvéghy <alexanderhidveghy@Mac.home>
2026-03-25 13:14:23 +0100

    feat: add root-level rewrites for all templates

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:slug(alumsist|austav|avrekon|bomif|dobritesari|efektivnestavby|ellio|hurtstav|hydrotunel|inrestsro|kosap|kostelansky|kronostav|la-ma|miroslavholecvykopoveprace|mmsadrokartony|poterymm|pramo|premona-nitra|purdom|redosi_group|revast|soki|soultrade|stamaxplus|starbuilding|stavferka|vo-vyske|zamkovadlazbamartin)',
+        destination: '/templates/:slug/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
